445141ab7183751ea3a21ee9c79abcb987a6f999
Vanka Siddhartha <97089805+vankaSiddhartha@users.noreply.github.com>
2025-03-14 16:02:40 +0530

    gcd impl

---
+++ STL/STL_impl/gcd.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int gcd(int a, int b){
+    if(b==0){
+        return a;
+    }
+    return gcd(b,a%b);
+
+}
+signed main(){
+  cout<<"gcd of 10 20 "<<gcd(10,20)<<"\n";
+}
