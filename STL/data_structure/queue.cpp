6dcc579bf0d357258a216029b24f0f1b402a3214
Vanka Siddhartha <97089805+vankaSiddhartha@users.noreply.github.com>
2025-03-15 16:57:20 +0530

    queue

---
+++ STL/data_structure/queue.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+signed main(){
+    //fifo
+    queue<int> q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    cout<<q.front()<<"\n" ;// gives front 
+    q.pop() ;// remove front element;
+
+
+
+}
